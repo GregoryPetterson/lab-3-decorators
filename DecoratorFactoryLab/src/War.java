9eb3d7717b7c0f1b614c53a51ddad9cb0fd92d2d
Gregory Peterson <pet03356@morris.umn.edu>
2024-02-17 11:12:14 -0600

    Added exception for when there aren't enough cards in the deck.

--- DecoratorFactoryLab/src/War.java
+++ DecoratorFactoryLab/src/War.java
@@ -8,29 +8,29 @@ public class War extends Game{
                 deck.add(new RegularCard(suit, rank));
             }
         }
-        toDeal = 27;
+        toDeal = 27; 
         minPlayers = 2;
         maxPlayers = 2;
     }
 
     @Override
-public ArrayList<ArrayList<Card>> deal(int n) {
-    ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>();
-    for (int i = 0; i < n; i++) {
-        hands.add(new ArrayList<Card>());
-    }
-
-    for (int i = 0; i < toDeal; i++) {
-        for (ArrayList<Card> hand : hands) {
-            if (deck.size() > 0) { // Check if there are cards left in the deck
-                hand.add(deck.remove(deck.size() - 1));
-            } else {
-                System.out.println("Not enough cards in the deck to deal");
-                return hands;
-            }
+    public ArrayList<ArrayList<Card>> deal(int n) {
+        if (!(n == minPlayers)) {
+            throw new RuntimeException("Only two players can play can play War.");
         }
-    }
+        if (n * toDeal > 52) {
+            throw new RuntimeException("Not enough cards in the deck.");
+        }
 
-    return hands;
-}
+        ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>(); // create a list of hands
+        for (int i = 0; i < n; i++) {
+            ArrayList<Card> hand = new ArrayList<Card>(); // create a hand
+            for (int j = 0 ; j < toDeal; j++) {
+                hand.add(deck.remove(0)); // remove the first card from the deck and add it to the hand
+            }
+            hands.add(hand); // add the hand to the list of hands    
+        }
+        System.out.println(hands);
+        return hands;
+    }   
 }
