1027d0f43527308570f697b3a3075c31de9a143e
Gregory Peterson <pet03356@morris.umn.edu>
2024-02-16 20:41:22 -0600

    Debugging War()

--- DecoratorFactoryLab/src/GameTest.java
+++ DecoratorFactoryLab/src/GameTest.java
@@ -1,22 +1,32 @@
 public class GameTest {
     public static void main(String[] args) {
         GameStore gameStore = new GameStore();
-        Game game = gameStore.createGame("Poker");
+        // Game game = gameStore.createGame("Poker");
         
-        game.shuffle();
-        game.deal(2);
-        game.printDeck();
+        // game.shuffle();
+        // game.deal(2);
+        // game.printDeck();
 
-        // Print the hands
-        for (int i = 0; i < 2; i++) {
-            // print the player number (I added + 1 because it makes more sense to start from player 1 instead of 0)
-            System.out.println("Player " + (i+1) + " has the following hand:");
-            for (Card card: game.deal(2).get(i)) {
-                System.out.println(card);
-            }
-        }
+        // // Print the hands
+        // for (int i = 0; i < 2; i++) {
+        //     // print the player number (I added + 1 because it makes more sense to start from player 1 instead of 0)
+        //     System.out.println("Player " + (i+1) + " has the following hand:");
+        //     for (Card card: game.deal(2).get(i)) {
+        //         System.out.println(card);
+        //     }
+        // }
 
-        Game War = gameStore.createGame("War");
+        Game war = gameStore.createGame("War");
+
+        war.shuffle();
+        war.deal(2);
+
+        // for (int i = 0; i < 2; i++) {
+        //     System.out.println("Player " + (i+1) + " has the following hand:");
+        //     for (Card card: war.deal(2).get(i)) {
+        //         System.out.println(card);
+        //     }
+        // }
 
         
 
--- DecoratorFactoryLab/src/War.java
+++ DecoratorFactoryLab/src/War.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+
 public class War extends Game{
+    public War() {
+        deck = new ArrayList<Card>();
+        for (String suit : RegularCard.suits) {
+            for (String rank : RegularCard.ranks) {
+                deck.add(new RegularCard(suit, rank));
+            }
+        }
+        toDeal = 2; // Only works for 5
+        minPlayers = 2;
+        maxPlayers = 2;
+    }
+
+    @Override
+    public ArrayList<ArrayList<Card>> deal(int n) {
+        // if (!(n == minPlayers)) {
+        //     throw new RuntimeException("Only two players can play can play War.");
+        // }
+
+        ArrayList<ArrayList<Card>> hands = new ArrayList<ArrayList<Card>>(); // create a list of hands
+        for (int i = 0; i < n; i++) {
+            ArrayList<Card> hand = new ArrayList<Card>(); // create a hand
+            for (int j = 0 ; j< toDeal; j++) {
+                hand.add(deck.remove(0)); // remove the first card from the deck and add it to the hand
+            }
+            hands.add(hand); // add the hand to the list of hands
+            
+        }
+        System.out.println(hands);
+        return hands;
+    }
     
 }
